cb466aaf75f804e66a30ae0d9ec20fd7748f6d2e
Ethan Zhang <ethan.zhang@ringcentral.com>
2022-08-09 16:26:27 +0800

    refactor(*) refactor

--- src/modules/notification/notification.module.ts
+++ src/modules/notification/notification.module.ts
@@ -1,11 +1,22 @@
 import { Module } from '@nestjs/common';
-import { createNotificationWhenPaymentIsCreatedProvider } from './providers/notification.providers';
+import {
+  createNotificationWhenPaymentIsCreatedProvider,
+  ServiceProviders,
+} from './providers/notification.providers';
+import { NotificationSmsAdapter } from '@infrastructure/adapters/notification-sms.adapter';
 
 const eventHandlers = [createNotificationWhenPaymentIsCreatedProvider];
+
+const serviceProviders = [
+  {
+    provide: ServiceProviders.NOTIFICATION_ADAPTER,
+    useClass: NotificationSmsAdapter,
+  },
+];
 
 @Module({
   imports: [],
   controllers: [],
-  providers: [...eventHandlers],
+  providers: [...eventHandlers, ...serviceProviders],
 })
 export class NotificationModule {}
--- src/modules/notification/providers/notification.providers.ts
+++ src/modules/notification/providers/notification.providers.ts
@@ -1,20 +1,25 @@
 import { Provider } from '@nestjs/common';
 import { UnitOfWork } from '@src/infrastructure/database/unit-of-work/unit-of-work';
 import { CreateNotificationWhenPaymentIsCreatedDomainEventHandler } from '../event-handlers/create-notification-when-payment-is-created.domain-event-handler';
-import { NotificationSmsAdapter } from '@infrastructure/adapters/notification-sms.adapter';
+import { NotificationAdapterPort } from '@modules/notification/adapters/notification.adapter.port';
+
+export const ServiceProviders = {
+  NOTIFICATION_ADAPTER: 'NOTIFICATION_ADAPTER',
+};
 
 export const createNotificationWhenPaymentIsCreatedProvider: Provider = {
   provide: CreateNotificationWhenPaymentIsCreatedDomainEventHandler,
   useFactory: (
     unitOfWork: UnitOfWork,
+    notificationAdapter: NotificationAdapterPort,
   ): CreateNotificationWhenPaymentIsCreatedDomainEventHandler => {
     const eventHandler =
       new CreateNotificationWhenPaymentIsCreatedDomainEventHandler(
         unitOfWork,
-        new NotificationSmsAdapter(),
+        notificationAdapter,
       );
     eventHandler.listen();
     return eventHandler;
   },
-  inject: [UnitOfWork],
+  inject: [UnitOfWork, ServiceProviders.NOTIFICATION_ADAPTER],
 };
